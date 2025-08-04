e7f113f4cc7294450f89d384d02863db2bdab6f6
Sarah N Melvin <sarah_n_melvin@carmax.com>
2025-08-04 16:45:11 -0400

    add dev cors policy

--- src/backend/Cta.Exercise.Service/Program.cs
+++ src/backend/Cta.Exercise.Service/Program.cs
@@ -31,6 +31,9 @@ builder.Services.AddCors(options =>
             policy.WithOrigins("http://localhost:5173")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
+            policy.WithOrigins("https://yellow-tree-0d394590f.2.azurestaticapps.net/")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
         });
 });
 
